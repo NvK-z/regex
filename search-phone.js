ac361d207cbf9bbfa696a413a065f1c4455c0056
Kevin Costa <midas159@outlook.com>
2026-01-18 02:08:58 -0300

    Inserindo o search-cpf

--- search-phone.js
+++ search-phone.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 const bancoCsv = 'database.csv';
 const banco = fs.readFileSync(bancoCsv, 'utf-8');
 
-const regexPhone = /\(\d+\)\s\d+-\d+/g;
+const regexPhone = /\(\d{2}\)\s\d{4,5}-\d{4}/g;
 
 const matchRegexPhone = banco.match(regexPhone);
 console.log(matchRegexPhone);
+
+const patternCel = /\(\d{2}\)\s\d{5}-\d{4}/g;
+
+const matchRegexCel = banco.match(patternCel);
+console.log(matchRegexCel);
--- search.js
+++ search.js
@@ -3,6 +3,6 @@ const bancoCsv = 'database.csv';
 const banco = fs.readFileSync(bancoCsv, 'utf-8');
 
 const regex = /Anna/;
-const matchRegex = banco.match(regex);
 
+const matchRegex = banco.match(regex);
 console.log(matchRegex);
