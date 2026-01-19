e5516a7da1cd94c0b651c9527f1e276523e03193
Kevin Costa <midas159@outlook.com>
2026-01-19 02:30:12 -0300

    Aperfeiçoando o search-name

--- search-name.js
+++ search-name.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const bancoCsv = 'database.csv';
 const banco = fs.readFileSync(bancoCsv, 'utf-8');
 
-const regexNames = /^([A-Za-zÀ-ÿ]+)(\s[A-Za-zÀ-ÿ]+)+/gm;
+const regexNames = /^([A-Za-zÀ-ÿ]+)(?:\s([A-Za-zÀ-ÿ]+))+/gm;
 
 const matchRegexNames = banco.match(regexNames);
 console.log(matchRegexNames);
